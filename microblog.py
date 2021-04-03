32903712da6f979d2c8d8adf04204937c31f84ed
Andrei <inwaves@live.com>
2021-04-03 23:32:10 +0300

    Finish P4

--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Task
+
+@app.shell_context_processor
+def make_shell_context():
+    """Creates a `flask` shell context for this DB instance and models."""
+    return {"db": db, "User": User, "Task": Task}
